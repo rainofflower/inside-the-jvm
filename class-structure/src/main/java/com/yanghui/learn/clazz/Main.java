d6dcf298633fa4b217e32f0669e39195656293fb
yanghui <yanghui@linklogis.com>
2020-05-19 18:17:35 +0800

    类文件结构

---
+++ class-structure/src/main/java/com/yanghui/learn/clazz/Main.java
@@ -0,0 +1,14 @@
+package com.yanghui.learn.clazz;
+
+/**
+ *
+ * @author yanghui
+ * @date 2020-05-19 16:43
+ **/
+public class Main {
+
+    public static void main(String... a){
+        TestClass testClass = new TestClass();
+        System.out.println(testClass.inc());
+    }
+}
---
+++ class-structure/src/main/java/com/yanghui/learn/clazz/TestClass.java
@@ -0,0 +1,14 @@
+package com.yanghui.learn.clazz;
+
+/**
+ * @author yanghui
+ * @date 2020-05-19 16:40
+ **/
+public class TestClass {
+
+    private int m;
+
+    public int inc(){
+        return m + 1;
+    }
+}
